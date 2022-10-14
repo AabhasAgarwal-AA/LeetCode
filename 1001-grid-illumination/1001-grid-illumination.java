8401fb0a4ad3fb183e6b57da5226cfcf7812640e
Aabhas Agarwal <81240819+AabhasAgarwal-AA@users.noreply.github.com>
2022-10-15 00:15:48 +0530

    Time: 89 ms (86.96%), Space: 58.4 MB (88.41%) - LeetHub

---
+++ 1001-grid-illumination/1001-grid-illumination.java
@@ -0,0 +1,65 @@
+class Solution {
+    // int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
+    int[][] dirs = {
+        {0, 0},
+        {-1, 0},
+        {-1, 1},
+        {0, 1},
+        {1, 1},
+        {1, 0},
+        {1, -1},
+        {0, -1},
+        {-1, -1},
+    };
+    public int[] gridIllumination(int n, int[][] lamps, int[][] queries) {
+        HashMap<Integer, Integer> rowMap = new HashMap<>();
+        HashMap<Integer, Integer> colMap = new HashMap<>();
+        HashMap<Integer, Integer> xpyMap = new HashMap<>();
+        HashMap<Integer, Integer> xmyMap = new HashMap<>();
+        HashMap<Integer, Integer> lampMap = new HashMap<>();
+        
+        for(int[] lamp : lamps){
+            int x = lamp[0], y = lamp[1];
+            if(!lampMap.containsKey(x * n + y)){
+                rowMap.put(x, rowMap.getOrDefault(x, 0) + 1);
+                colMap.put(y, colMap.getOrDefault(y, 0) + 1);
+                xpyMap.put(x + y, xpyMap.getOrDefault(x + y, 0) + 1);
+                xmyMap.put(x - y, xmyMap.getOrDefault(x - y, 0) + 1);
+                lampMap.put(x * n + y, lampMap.getOrDefault(x * n + y, 0) + 1);
+            }
+        }
+        
+        int[] ans = new int[queries.length];
+        
+        for(int k = 0; k < queries.length; k++){
+            int x = queries[k][0];
+            int y = queries[k][1];
+            
+            if(rowMap.getOrDefault(x, 0) > 0){
+                ans[k] = 1;
+            }else if(colMap.getOrDefault(y, 0) > 0){
+                ans[k] = 1;
+            }else if(xpyMap.getOrDefault(x + y, 0) > 0){
+                ans[k] = 1;
+            }else if(xmyMap.getOrDefault(x - y, 0) > 0){
+                ans[k] = 1;
+            }
+            for(int[] dir : dirs){
+                int i = x + dir[0];
+                int j = y + dir[1];
+                if(i < 0 || i >= n || j < 0 || j >= n)
+                    continue;
+                
+                if(lampMap.containsKey(i * n + j)){
+                    rowMap.put(i, rowMap.getOrDefault(i, 0) - 1);
+                    colMap.put(j, colMap.getOrDefault(j, 0) - 1);
+                    xpyMap.put(i + j, xpyMap.getOrDefault(i + j, 0) - 1);
+                    xmyMap.put(i - j, xmyMap.getOrDefault(i - j, 0) - 1);
+                    lampMap.remove(i * n + j);
+                }
+            }
+            
+        }
+        return ans;
+    }
+}
