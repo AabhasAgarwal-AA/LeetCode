0379ec580c9e1ec498a0599604c0d759aaa241cc
Aabhas Agarwal <81240819+AabhasAgarwal-AA@users.noreply.github.com>
2022-10-22 20:41:21 +0530

    Time: 2 ms (96.91%), Space: 42.6 MB (95.20%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -2,31 +2,34 @@ class Solution {
     public int closedIsland(int[][] grid) {
         int count = 0;
         
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 0){ 
+        for(int i = 1; i < grid.length - 1; i++){
+            for(int j = 1; j < grid[0].length - 1; j++){
+                if(grid[i][j] == 0){
                     if(dfs(grid, i, j))
-                        count++;   
+                        count++;
                 }
             }
         }
         
         return count;
     }
-    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1},{0, -1}};
-    
+    int[][] dirs = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
     private boolean dfs(int[][] grid, int i, int j){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length)
+        if(i == 0 || i == grid.length - 1 || j == 0 || j == grid[0].length - 1)
             return false;
         
         if(grid[i][j] == 1 || grid[i][j] == 2)
             return true;
-        grid[i][j] = 2;
         
-        boolean ans = true;
+        grid[i][j] = 2;
+        boolean res = true;
         for(int[] dir: dirs){
-           ans = dfs(grid, i + dir[0], j + dir[1]) && ans;
+            int r = i + dir[0];
+            int c = j + dir[1];
+            if(grid[r][c] == 0){ 
+                res = dfs(grid, r, c) && res;
+            }
         }
-        return ans;
+        return res;
     }
 }
