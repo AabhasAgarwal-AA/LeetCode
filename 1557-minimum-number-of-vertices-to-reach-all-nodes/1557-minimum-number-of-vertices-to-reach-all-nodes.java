80bfcfaa92d89ec41bd09d3b59b79863c570bcd0
Aabhas Agarwal <81240819+AabhasAgarwal-AA@users.noreply.github.com>
2022-10-27 12:14:50 +0530

    Time: 25 ms (46.43%), Space: 118.9 MB (77.49%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,36 +1,17 @@
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        int[] parent = new int[n];
-        boolean[] vis = new boolean[n];
+        int[] inDegree = new int[n];
         
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-        }
-        
-        for(int i = 0; i < edges.size(); i++){
-            List<Integer> edge = edges.get(i);
-            int u = edge.get(0), v = edge.get(1);
-            
-            if(vis[v])
-                continue;
-            vis[v] = true;
-            
-            int p1 = findPar(u, parent);
-            int p2 = findPar(v, parent);
-            
-            if(p1 != p2)
-                parent[p2] = p1;
+        for(List<Integer> edge: edges){
+            inDegree[edge.get(1)] = 1;
         }
         
         List<Integer> list = new ArrayList<>();
         for(int i = 0; i < n; i++){
-            if(parent[i] == i)
+            if(inDegree[i] == 0)
                 list.add(i);
         }
+        
         return list;
-    }
-    
-    private int findPar(int u, int[] parent){
-        return parent[u] == u ? u : (parent[u] = findPar(parent[u], parent));
     }
 }
