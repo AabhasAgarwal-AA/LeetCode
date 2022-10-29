5a6e5ded85afd026c82e9fb9aa03c7c4dc70985a
Aabhas Agarwal <81240819+AabhasAgarwal-AA@users.noreply.github.com>
2022-10-29 14:57:00 +0530

    Time: 66 ms (83.46%), Space: 106.9 MB (68.64%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,28 +1,26 @@
 class Solution {
     int count = 0;
     public int minReorder(int n, int[][] connections) {
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i < n; i++)
-            graph.add(new ArrayList<>());
-        
-        for(int i = 0; i < connections.length; i++){
-            int u = connections[i][0], v = connections[i][1];
-            
-            graph.get(u).add(v);
-            graph.get(v).add(-u);
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        for(int i = 0; i < n; i++){
+            graph[i] = new ArrayList<>();
         }
-        
-        
+        for(int[] conn: connections){
+            int u = conn[0], v = conn[1];
+            graph[u].add(v);
+            graph[v].add(-u);
+        }
         boolean[] vis = new boolean[n];
         dfs(graph, vis, 0);
         return count;
     }
-    private void dfs(ArrayList<ArrayList<Integer>> graph, boolean[] vis, int v){
+    
+    private void dfs(ArrayList<Integer>[] graph, boolean[] vis, int v){
         vis[v] = true;
         
-        List<Integer> nbrs = graph.get(v);
+        List<Integer> nbrs = graph[v];
         
-        for(int nbr  : nbrs){
+        for(int nbr: nbrs){
             if(!vis[Math.abs(nbr)]){
                 if(nbr > 0)
                     count++;
