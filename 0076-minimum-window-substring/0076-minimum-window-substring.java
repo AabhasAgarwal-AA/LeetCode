70a9619e5f374ef3d8933c342bfafceba6b05a5a
Aabhas Agarwal <81240819+AabhasAgarwal-AA@users.noreply.github.com>
2022-11-02 15:42:58 +0530

    Time: 208 ms (10.86%), Space: 42.9 MB (87.05%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -7,27 +7,29 @@ class Solution {
         }
         
         int j = -1;
-        HashMap<Character, Integer> smap = new HashMap<>();
         int count = 0;
+        Map<Character, Integer> smap = new HashMap<>();
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
+            
             smap.put(ch, smap.getOrDefault(ch, 0) + 1);
             if(smap.getOrDefault(ch, 0) <= tmap.getOrDefault(ch, 0))
                 count++;
             
             while(count == t.length() && j < i){
                 String ans = s.substring(j+1, i+1);
-                if(res.length() == 0 || res.length() > ans.length()){
+                if(res.length() == 0 || res.length() > ans.length())
                     res = ans;
-                }
                 
                 j++;
                 char chr = s.charAt(j);
-                smap.put(chr, smap.get(chr) - 1);
+                smap.put(chr, smap.getOrDefault(chr, 0) - 1);
                 if(smap.getOrDefault(chr, 0) < tmap.getOrDefault(chr, 0))
                     count--;
             }
         }
+        
+        
         return res;
     }
 }
