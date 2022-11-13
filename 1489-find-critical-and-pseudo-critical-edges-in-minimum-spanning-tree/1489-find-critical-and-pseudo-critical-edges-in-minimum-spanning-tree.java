0b602c049f8f34aa0ecf041a389fab33892a2a6c
Aabhas Agarwal <81240819+AabhasAgarwal-AA@users.noreply.github.com>
2022-11-13 20:39:44 +0530

    Time: 409 ms (5.08%), Space: 82.6 MB (5.08%) - LeetHub

---
+++ 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.java
@@ -0,0 +1,145 @@
+class Solution {
+    class unionFind{
+        int[] p, r;
+        int comps = 0;
+        unionFind(int n){
+            p = new int[n];
+            r = new int[n];
+            comps = n;
+            for(int i = 0; i < p.length; i++){
+                p[i] = i;
+                r[i] = 0;
+            }
+        }
+        int find(int x){
+            if(p[x]== x)
+                return x;
+            else{
+                p[x] = find(p[x]);
+                return p[x];
+            }
+        }
+        boolean union(int X, int Y){
+            int x = find(X);
+            int y = find(Y);
+            if(x == y)
+                return false;
+            
+            if(r[x] < r[y]){
+                p[x] = y;
+            }else if(r[y] < r[x]){
+                p[y] = x;
+            }else{
+                p[y] = x;
+                r[x]++;
+            }
+            comps--;
+            return true;
+        }
+        boolean isConnected(){
+            return comps == 1;
+        }
+    }
+    
+    class Edge implements Comparable<Edge>{
+        int u, v, wt;
+        Edge(int u, int v, int wt){
+            this.u = u;
+            this.v = v;
+            this.wt = wt;
+        }
+        public int compareTo(Edge o){
+            return this.wt - o.wt;
+        }
+    }
+    
+    private int buildMST(int n, int[][] edges, int[] edgeToSkip, int[] edgeToPick){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        for(int[] edge: edges){
+            if(edge == edgeToSkip)
+                continue;
+            else if(edge == edgeToPick)
+                continue;
+            
+            int u = edge[0], v = edge[1], wt = edge[2];
+            pq.add(new Edge(u, v, wt));
+        }
+        
+        unionFind uf = new unionFind(n);
+        int cost = 0;
+        if(edgeToPick != null){
+            uf.union(edgeToPick[0], edgeToPick[1]);
+            cost += edgeToPick[2];
+        }
+        while(pq.size() > 0){
+            Edge rem = pq.remove();
+            if(uf.union(rem.u, rem.v) == true)
+                cost += rem.wt;
+        }
+        if(uf.isConnected() == true)
+            return cost;
+        
+        return Integer.MAX_VALUE;
+    } 
+
+    public List<List<Integer>> findCriticalAndPseudoCriticalEdges(int n, int[][] edges) {
+        int mstCost = buildMST(n, edges, null, null);
+        List<Integer> critical = new ArrayList<>();
+        List<Integer> pcritical = new ArrayList<>();
+        
+        for(int i = 0; i < edges.length; i++){
+            int[] edge = edges[i];
+            int mstCostWithoutEdge = buildMST(n, edges, edge, null);
+            
+            if(mstCostWithoutEdge > mstCost)
+                critical.add(i);
+            else{
+                int mstCostWithEdge = buildMST(n, edges, null, edge);
+                
+                if(mstCostWithEdge > mstCost){
+                    // redundant
+                }else{
+                    pcritical.add(i);
+                }
+            }
+        }
+        
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(critical);
+        res.add(pcritical);
+        return res;
+    }
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
